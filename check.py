6abfa4d434164eac9af7cde676e16bd2e72f0e61
Sedherthe <siddhartha.soma1@gmail.com>
2021-02-18 14:32:56 +0000

    Soma | Add a check file for verification

---
+++ check.py
@@ -0,0 +1,29 @@
+from configs.train_config import SpectConfig
+from loader.data_loader import SpectrogramDataset, collate_fn
+from models.model import Conformer, get_conv_output_sizes
+import torch
+from torch.utils.data import DataLoader
+
+if __name__ == "__main__":
+    spect_cfg = SpectConfig()
+    songs_dset = SpectrogramDataset('/home/soma/song_speech/speech', 1, spect_cfg)
+    print(len(songs_dset))
+    feat, label = songs_dset[1000]
+    print(feat.shape, label) # [257, T]
+
+    model = Conformer()
+    batch = feat.unsqueeze(0)
+    print(batch.shape)
+
+    out = model(batch)
+    print("out shape: ", out.shape)
+
+    lengths = get_conv_output_sizes([feat.shape[1]])
+    print('conv out lengths: ', lengths)
+
+    loader = DataLoader(songs_dset, batch_size=10, collate_fn=collate_fn)
+    print('data loader len: ', len(loader))
+
+    mini_batch = iter(loader).next()
+    out = model(mini_batch[0])
+    print('mini batch output ', out.shape)
