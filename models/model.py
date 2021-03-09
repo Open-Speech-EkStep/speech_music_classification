84c2d0d362610b1d27ad491cfc17a8c5e1f41906
Sedherthe <siddhartha.soma1@gmail.com>
2021-03-09 13:50:06 +0000

    Soma | Add final aggregations layers using mean, var

--- models/model.py
+++ models/model.py
@@ -27,7 +27,7 @@ class TransformerBlock(nn.Module):
         self.transformer = nn.TransformerEncoder(self.encoder_layer, num_layers=3)
 
     def forward(self, x, src_key_mask):
-        out = self.transformer(src, src_key_padding_mask=src_key_mask)
+        out = self.transformer(x, src_key_padding_mask=src_key_mask)
         return out
 
 class Conformer(nn.Module):
@@ -42,14 +42,32 @@ class Conformer(nn.Module):
             self.conv_blocks.append(ConvolutionBlock(num_in_filter, num_out_filter))
 
         self.transformer = TransformerBlock()
+        self.linear1 = nn.Linear(self.out_filters[-1]*2, 128)
+        self.linear2 = nn.Linear(128, 2)
 
-    def forward(self, x):
-        print("Forward Pass")
+
+    def forward(self, x, attn_mask, lengths):
+        
+        # ConvBlock - Forward Pass
         for conv_block in self.conv_blocks:
             x = conv_block(x)
             print(x.shape)
 
         out = x.permute(2, 0, 1) # [T, B, E]
+
+        # Transformer Block - Forward pass
+        out = self.transformer(out, attn_mask)
+        #print("output after transformer shape is ", out.shape)
+
+        # Statistics Pooling Layer
+        stats = out.new_zeros((out.shape[1], out.shape[2]*2)) # [B, 2*E]
+        for i, l in enumerate(lengths):
+            mean = torch.mean(out[:l, i, :], dim=0)
+            var = torch.var(out[:l, i, :], dim=0)
+            stats[i] = torch.cat([mean, var])
+
+        out = self.linear1(stats)
+        out = self.linear2(out)
         return out
 
 
